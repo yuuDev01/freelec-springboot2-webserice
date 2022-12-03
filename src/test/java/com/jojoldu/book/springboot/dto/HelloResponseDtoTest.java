9e63829b33373eeb9d9936ac2ce13ab61e332298
yuuDev01 <pasenoaz@gmail.com>
2022-12-04 01:53:35 +0900

    lombok 적용/dto추가

---
+++ src/test/java/com/jojoldu/book/springboot/dto/HelloResponseDtoTest.java
@@ -0,0 +1,20 @@
+package com.jojoldu.book.springboot.dto;
+
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+public class HelloResponseDtoTest {
+    @Test
+    public void 룸복_테스트(){
+        //전송
+        String name = "test";
+        int amount = 1000;
+
+        //언제
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
